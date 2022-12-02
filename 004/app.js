344d4afa1c8114f8203a65fa4defd569f3035375
João Vitor Polizelli <jvitorpzl@gmail.com>
2022-12-01 22:16:58 -0300

    exercicio 6
    começando ex 6  de javascrip´t

--- 004/app.js
+++ 004/app.js
@@ -1,8 +1,22 @@
-function VerificarDia()
-{
-    let dia = document.getElementById('dia').value 
-    dayName = new Array ("domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado")
-
-    ("<h1> Hoje é " + dayName[dia.getDay() ] )    
-    document.getElementById("resultado").innerHTML = dayName[d.getDay()];
+function VerificarDia(){
+    let dia = document.getElementById("dia").value 
+    if(dia == "1"){
+        document.getElementById("resultado").innerHTML = "Segunda-feira"
+    }else if(dia == "2"){
+        document.getElementById("resultado").innerHTML = "Terça-Feira"
+    }else if(dia == "3"){
+        document.getElementById("resultado").innerHTML = "Quarta-Feira"
+    }else if(dia == "4"){
+        document.getElementById("resultado").innerHTML = "Quinta-Feira"
+    }else if(dia == "5"){
+        document.getElementById("resultado").innerHTML = "Sexta-Feira"
+    }else if(dia == "6"){
+        document.getElementById("resultado").innerHTML = "Sábado"
+    }else if(dia == "7"){
+        document.getElementById("resultado").innerHTML = "Domingo"
+    }
+    else{
+        document.getElementById("resultado").innerHTML = "Valor inválido"
+    }
 }
+
